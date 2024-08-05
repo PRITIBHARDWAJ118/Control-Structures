4e82c48d41e6ed27fd646a68aafb5c2f830e422e
Priti <pritibhardwaj118@gmail.com>
2024-08-05 23:07:58 +0530

    Add files via upload

---
+++ odd_even_numbers.cpp
@@ -0,0 +1,21 @@
+//Priti
+//23070123103
+//exp5- even or odd numbers
+#include<iostream>
+using namespace std;
+int main()
+{
+    int num;
+    cout<<"Enter a number: ";
+    cin>>num;
+    if(num%2==0)
+    {
+        cout<<num<<" is an even number ";
+
+    }
+    else 
+    {
+        cout<<num<<" is a odd number";
+    }
+    return 0;
+}
